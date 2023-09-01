33addf5c4ea9ccfe1de60dd4ac1ee893012782e4
Friedrich Pahlke <friedrich.pahlke@rpact.com>
2023-09-01 08:22:07 +0200

    New function as251Normal() added incl. unit tests

--- R/f_as251.R
+++ R/f_as251.R
@@ -1,3 +1,5 @@
+
+#' @export 
 mvnprd <- function(A, B, BPD, EPS = 1e-06, INF, IERC = 1, HINC = 0) {
     if (length(A) != length(B) || length(B) != length(BPD) || length(BPD) != length(INF)) {
         stop("Input vectors must have the same length.")
@@ -9,4 +11,14 @@ mvnprd <- function(A, B, BPD, EPS = 1e-06, INF, IERC = 1, HINC = 0) {
         warning("mvnprd returned a fault.")
     }
     return(result)
+}
+
+#' @export 
+as251Normal <- function(lower, upper, sigma, inf, ..., eps = 1e-06, ierc = 1, hinc = 0) {
+    bpd <- sigmaToBPD(sigma)
+    n <- length(bpd)
+    lower <- rep(lower, n)
+    upper <- rep(upper, n)
+    inf <- lower
+    mvnprd(lower, upper, bpd, eps, inf, ierc, hinc)
 }
--- tests/testthat/test-f_as251.R
+++ tests/testthat/test-f_as251.R
@@ -14,3 +14,26 @@ test_that("mvnprd and sadmvn are equal", {
     x2 <- mnormt::sadmvn(lower = B, upper = A, mean = 0, varcov = sigma)
     expect_equal(x1[1], x2[1], tolerance = eps)
 })
+
+
+test_that("mvnprd and sadmvn are equal", {
+    A <- rep(2, 3)
+    B <- rep(-3, 3)
+    BPD <- sqrt(c(0.3, 0.4, 0.5))
+    eps <- 1e-06
+    INF <- rep(2, 3)
+    ierc <- 0
+    hinc <- 0
+    x1 <- mvnprd(A = A, B = B, BPD = BPD, EPS = eps, INF = INF, IERC = ierc, HINC = hinc)
+
+    sigma <- BPD %*% t(BPD)
+    diag(sigma) <- 1
+
+    x2 <- mnormt::sadmvn(lower = B, upper = A, mean = 0, varcov = sigma)
+    
+    
+    x3 <- as251Normal(lower = 2, upper = -3, sigma = sigma, inf = INF, eps = eps, ierc = ierc, hinc = 0)
+    
+    expect_equal(x1[1], x2[1], tolerance = eps)
+    expect_equal(x1[1], x3[1], tolerance = eps)
+})
