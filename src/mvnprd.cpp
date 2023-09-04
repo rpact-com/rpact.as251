21d06419423659bb76d7bbfaa24eefb44b0d5b67
Till Luca Jensen <till.luca.jensen@stud.th-luebeck.de>
2023-09-04 12:14:50 +0200

    fixed variable length array, added comments, removed unneccessary if block

--- src/mvnprd.cpp
+++ src/mvnprd.cpp
@@ -12,25 +12,30 @@ extern "C" {
 // [[Rcpp::export(".mvnprd")]]
 NumericVector mvnprd(NumericVector a, NumericVector b, NumericVector bpd, float eps, IntegerVector inf, int ierc, float hinc) {
   int n = a.size();
-  if(a.size() != b.size() && b.size() != bpd.size() && bpd.size() != inf.size()) {
-    //TODO error
-  }
+
+  //initialize float arrays
+  float* af = new float[n];
+  float* bf = new float[n];
+  float* bpdf = new float[n];
 
   //copy vectors into float arrays
-  float af[n];
-  float bf[n];
-  float bpdf[n];
-
   for(int i = 0; i < n; i++) {
     af[i] = (float) a[i];
     bf[i] = (float) b[i];
     bpdf[i] = (float) bpd[i];
   }
 
+  //initialize result variables
   float prob = 0;
   float bound = 0;
   int ifault = 0;
+
   mvnprd_(&(af[0]), &(bf[0]), &(bpdf[0]), &eps, &n, &(inf[0]), &ierc, &hinc, &prob, &bound, &ifault);
+
+  //free allocated memory
+  delete[] af;
+  delete[] bf;
+  delete[] bpdf;
 
   return NumericVector::create(prob, bound, ifault);
 }
