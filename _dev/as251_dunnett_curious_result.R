7df21f54b8c53d47f5483bf934738d668e51f88c
Friedrich Pahlke <friedrich.pahlke@rpact.com>
2023-09-14 08:21:53 +0200

    Curious example corrected

--- _dev/as251_dunnett_curious_result.R
+++ _dev/as251_dunnett_curious_result.R
@@ -5,7 +5,7 @@
 ## September 11, 2023
 ##
 
-upConst <- 0.95
+upConst <- 0.85
 sc <- 1.5
 gMax <- 6
 frac <- upConst * abs(sin((1:gMax)))^sc
@@ -14,5 +14,9 @@ sigma <- sqrt(frac) %*% sqrt(t(frac))
 diag(sigma) <- 1
 df <- 20
 upper <- 1.001
+mnormt::sadmvt(lower = -Inf, upper = upper, mean = 0, S = sigma, df = df)
+rpact.as251::as251StudentT(lower = -Inf, upper = upper, sigma = sigma, eps = 1e-05, df = df)
+
+upper <- 1
 mnormt::sadmvt(lower = -Inf, upper = upper, mean = 0, S = sigma, df = df)
 rpact.as251::as251StudentT(lower = -Inf, upper = upper, sigma = sigma, eps = 1e-05, df = df)
